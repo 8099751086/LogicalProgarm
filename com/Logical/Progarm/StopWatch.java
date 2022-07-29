13c708de0cb99c25333dff73914d621043cb66c1
8099751086 <dilwarhussainit24@gmail.com>
2022-07-30 00:11:23 +0530

    Hi

--- com/Logical/Progarm/StopWatch.java
+++ com/Logical/Progarm/StopWatch.java
@@ -2,17 +2,18 @@ package com.Logical.Progarm;
 
 import java.util.Scanner;
 
-public class LogicalProgram {
+public class StopWatch {
 
 	public static void main(String[] args) {
 		double startTime, stopTime;
 		System.out.print("Enter 1 To Start");
-		Scanner sc = new Scanner(System.in);
-		sc.nextInt();
-		startTime = System.currentTimeMillis();
-		System.out.println("start time " + startTime);
-		System.out.println("Enter 2 To Stop");
-		sc.nextInt();
+		try (Scanner sc = new Scanner(System.in)) {
+			sc.nextInt();
+			startTime = System.currentTimeMillis();
+			System.out.println("start time " + startTime);
+			System.out.println("Enter 2 To Stop");
+			sc.nextInt();
+		}
 		stopTime = System.currentTimeMillis();
 		System.out.println("stop time " + stopTime);
 		double timeElapsed = (stopTime - startTime) / 1000;
